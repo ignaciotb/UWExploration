5c49fff205818d2ecc7770a29151e4f74c6d0444
Hackerman342 <kwc57@cornell.edu>
2020-02-07 22:22:29 +0100

    publish pcl as PointCloud2 for visualization in RVIZ

--- auv_2_ros/src/auv2ros_parser.cpp
+++ auv_2_ros/src/auv2ros_parser.cpp
@@ -12,10 +12,30 @@
 #include "submaps_tools/submaps.hpp"
 #include "registration/utils_visualization.hpp"
 
+#include <ros/ros.h>
+#include <pcl_ros/point_cloud.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+// #include <sensor_msgs/PointCloud.h>
+// #include <pcl/point_cloud.h>
+// #include <pcl/point_types.h>
+// #include <pcl/PCLHeader.h>
+
 using namespace Eigen;
 using namespace std;
 
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloudpub;
+
 int main(int argc, char** argv){
+
+    // Initialize ROS node
+    ros::init(argc, argv, "point_cloud");
+    ros::NodeHandle n;
+    // Initialize PointCloud2 publisher
+    ros::Publisher pc_pub = n.advertise<sensor_msgs::PointCloud2> ("point_cloud_topic", 4);
+    // Create a container for PointCloud2 msg
+    sensor_msgs::PointCloud2 outcloud;
 
     // Inputs
     std::string folder_str, path_str, output_str, original, simulation;
@@ -70,17 +90,32 @@ int main(int argc, char** argv){
             us_filter.filter(*cloud_ptr);
             submap_i.submap_pcl_ = *cloud_ptr;
         }
-    }
 
+        // Convert point cloud to message for RVIZ viewing
+        pcl::toROSMsg(*cloud_ptr.get(),outcloud);
+
+        // Assign header as map to visualize in RVIZ        
+        outcloud.header.frame_id = "map";
+
+        // Publish point cloud message
+        ros::Rate loop_rate(.1);
+        while (n.ok()){
+            pc_pub.publish (outcloud);
+            loop_rate.sleep ();
+            }
+    }
     // Visualization on PCL
-    PCLVisualizer viewer ("Submaps viewer");
-    viewer.loadCameraParameters("Antarctica7");
-    SubmapsVisualizer* visualizer = new SubmapsVisualizer(viewer);
-    visualizer->setVisualizer(maps_gt, 1);
-    while(!viewer.wasStopped ()){
-        viewer.spinOnce ();
+    // bool vis = true;
+    bool vis = false;
+    if (vis){
+        PCLVisualizer viewer ("Submaps viewer");
+        viewer.loadCameraParameters("Antarctica7");
+        SubmapsVisualizer* visualizer = new SubmapsVisualizer(viewer);
+        visualizer->setVisualizer(maps_gt, 1);
+        while(!viewer.wasStopped ()){
+            viewer.spinOnce ();
+        }
+        viewer.resetStoppedFlag();
     }
-    viewer.resetStoppedFlag();
-
     return 0;
 }

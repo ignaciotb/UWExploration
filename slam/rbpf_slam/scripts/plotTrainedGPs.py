1d884f089ea3f346450dc2130b4384b4539a545f
stineolsson <stine@kth.se>
2021-04-14 14:00:37 +0200

    plotting data works

--- slam/rbpf_slam/scripts/plotTrainedGPs.py
+++ slam/rbpf_slam/scripts/plotTrainedGPs.py
@@ -8,6 +8,20 @@ import numpy as np
 import os
 
 # --------------- learning the basics ----------------
+# hej = 1
+# da = 1
+# new = np.empty((1,2))
+# print(new.shape)
+
+# new = np.append(new, [[hej,da]],axis=0)
+# hej = 2
+# da = 4
+# new = np.append(new, [[hej,da]],axis=0)
+
+# # new = np.append(da, axis=1)
+
+# print(new.shape)
+# print(new)
 # hej = np.array([[0, 1], [3, 4], [2,5]])
 # da = np.array([[6, 6],[7,7], [8,8]])
 # print(hej.shape)
@@ -22,39 +36,80 @@ import os
 # root = '/home/stine/catkin_ws/src/UWExploration/slam/rbpf_slam/data/'
 # cloud = np.load(root+'pcl_33_over.npy')
 # print(cloud.shape) # (2391787, 3)
-#plot input before training
-root = '/home/stine/catkin_ws/src/UWExploration/slam/rbpf_slam/data/record_pf2train_gp/results/particles/'
-_, _, names = next(os.walk(root))
-print(names[0])
-for f in names: 
-    p1 = np.load(root + f)
-    # print(p1.shape)
-    xx = p1[:,0]
-    yy = p1[:,1]
-    plt.plot(xx,yy, 'b')
-    plt.xlabel('x axis (m)')
-    plt.ylabel('y axis (m)')
+
+# class plotGPs():
+
+#     def __init__(self):
+
+def test_plot():
+    #plot input before training
+    root = '/home/stine/catkin_ws/src/UWExploration/slam/rbpf_slam/data/record_pf2train_gp/results/xy/'
+    _, _, names = next(os.walk(root))
+    print(names[0])
+    for f in names: 
+        p1 = np.load(root + f)
+        print('particle xy pose', p1.shape)
+        xx = p1[:,0]
+        yy = p1[:,1]
+        plt.plot(xx,yy, 'b')
+        plt.xlabel('x axis (m)')
+        plt.ylabel('y axis (m)')
+        plt.show()
+
+    root = '/home/stine/catkin_ws/src/UWExploration/slam/rbpf_slam/data/record_pf2train_gp/results/particles/'
+    _, _, names = next(os.walk(root))
+    print(names[0])
+    for f in names: 
+        p1 = np.load(root + f)
+        print('input', p1.shape)
+        xx = p1[:,0]
+        yy = p1[:,1]
+        plt.plot(xx,yy, 'b')
+        plt.xlabel('x axis (m)')
+        plt.ylabel('y axis (m)')
+        plt.show()
+
+def gp_data():    
+    # plot saved poserior
+    root = '/home/stine/catkin_ws/src/UWExploration/slam/rbpf_slam/data/particle_posterior/'
+    _, _, names = next(os.walk(root))
+    # print(names[0])
+    for f in names:
+        p1 = np.load(root + f)
+        print('after regression', p1.shape)
+        xx = p1[:,0]
+        yy = p1[:,1]
+        mu = p1[:,2]
+        sigma = p1[:,3]
+        # plot_fit(xx, yy, mu, sigma)
+        fig = plt.figure(figsize=(6,6), constrained_layout=True)
+        gs = fig.add_gridspec(3, 3)
+        ax1 = fig.add_subplot(gs[:2, :])
+        plt.plot(xx,yy, 'b')
+        plt.xlabel('x axis (m)')
+        plt.ylabel('y axis (m)')
+        ax2 = fig.add_subplot(gs[2, :])
+        plt.plot(mu,sigma, 'g')
+        plt.xlabel('mu')
+        plt.ylabel('sigma')
+        plt.show()
+
+def plot_fit(x,y,mu,var, m_y='k-o', m_mu='b-<', l_y='true', l_mu='predicted', legend=True, title=''):
+    """
+    Plot the fit of a GP
+    """
+    if y is not None:
+        plt.plot(x,y, m_y, label=l_y)
+    plt.plot(x,mu, m_mu, label=l_mu)
+    vv = 2*np.sqrt(var)
+    plt.fill_between(x, (mu-vv), (mu+vv), alpha=0.2)#, edgecolor='gray', facecolor='cyan')
+    # plt.fill_between(x[:,0], (mu-vv)[:,0], (mu+vv)[:,0], alpha=0.2, edgecolor='gray', facecolor='cyan')
+    if legend:
+        plt.legend()
+    if title != '':
+        plt.title(title)
     plt.show()
 
-# plot saved poserior
-root = '/home/stine/catkin_ws/src/UWExploration/slam/rbpf_slam/data/particle_posterior/'
-_, _, names = next(os.walk(root))
-# print(names[0])
-for f in names:
-    p1 = np.load(root + f)
-    # print(p1.shape)
-    xx = p1[:,0]
-    yy = p1[:,1]
-    mu = p1[:,2]
-    sigma = p1[:,3]
-    fig = plt.figure(figsize=(6,6), constrained_layout=True)
-    gs = fig.add_gridspec(3, 3)
-    ax1 = fig.add_subplot(gs[:2, :])
-    plt.plot(xx,yy, 'b')
-    plt.xlabel('x axis (m)')
-    plt.ylabel('y axis (m)')
-    ax2 = fig.add_subplot(gs[2, :])
-    plt.plot(mu,sigma, 'g')
-    plt.xlabel('mu')
-    plt.ylabel('sigma')
-    plt.show()
+if __name__ == '__main__':
+    # gp_data()
+    test_plot()
--- slam/rbpf_slam/scripts/train_pf_gp.py
+++ slam/rbpf_slam/scripts/train_pf_gp.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import rospy
+import torch
 from sensor_msgs.msg import PointCloud2
-from bathy_gps import gp
+import matplotlib.pyplot as plt
+from bathy_gps.gp import SVGP
 import numpy as np
 import os
 
@@ -15,10 +17,11 @@ class Train_gps():
         # self.pub = rospy.Publisher('/trained_gps', PointCloud2, queue_size=1)
         # Subscribe to particles
         # rospy.Subscriber('/gps2train', PointCloud2, self.cb)
+        # root = '/home/stine/catkin_ws/src/UWExploration/slam/rbpf_slam/data/'
+        # self.gp_path = root + 'svgp.pth'
         # ----------------------
-        root = '/home/stine/catkin_ws/src/UWExploration/slam/rbpf_slam/data/'
-        self.gp_path = root + 'svgp.pth'
-        self.data_path = root + 'record_pf2train_gp/results/'
+        
+        self.data_path = '/home/stine/catkin_ws/src/UWExploration/slam/rbpf_slam/data/record_pf2train_gp/results/'
         _, _, self.names = next(os.walk(self.data_path + 'particles/'))
         self.pc = len(self.names)
         self.particle = np.empty(self.pc, dtype=object)
@@ -29,22 +32,40 @@ class Train_gps():
     def trainNplot(self):
         # train particles gps
         targets = np.load(self.data_path + 'target_gp.npy')
-        for i in range (0,self.pc):
-            self.particle[i] = create_particle(self.gp_path)
+        for i in range (0,1):
+            self.particle[i] = create_particle()
             inputs = np.load(self.data_path + 'particles/' + self.names[i])
-            self.particle[i].gp.fit(inputs, targets, n_samples=6000, max_iter=1000, learning_rate=1e-1, rtol=1e-4, ntol=100, auto=False, verbose=True)
+            print(len(inputs))
+            print(len(targets))
+
+            self.particle[i].gp.fit(inputs, targets, n_samples=100, max_iter=100, learning_rate=1e-1, rtol=1e-4, ntol=100, auto=False, verbose=True)
             print('\nParticle {} done ({} in total) \n'.format(i, self.pc))
-            self.particle[i].gp.plot(inputs, targets,'../images/particle'+str(i)+'.png' )
-            self.particle[i].gp.save('../data/particle_gp_model/particle'+str(i)+'.pth')
+            self.particle[i].gp.plot(inputs, targets, self.data_path + 'training.png', n=100, n_contours=100 )
+            gp_path = self.data_path + 'svgp.pth'
+            self.particle[i].gp.save(gp_path)
             # save the posterior point cloud
-            x = inputs[:,0]
-            y = inputs[:,1]
-            self.particle[i].gp.save_posterior(1000, min(x), max(x), min(y), max(y),'../data/particle_posterior/particle'+str(i)+'.npy', verbose=False)
+            self.plot_and_save_posterior(inputs, targets, gp_path)
+            
+            # fig, ax = plt_settings()
+            # Regression_result(fig, ax)
+            # self.particle[i].gp.save_posterior(100, min(x), max(x), min(y), max(y),'../data/particle_posterior/particle'+str(i)+'.npy', verbose=False)
+
+    def plot_and_save_posterior(self, inputs, targets, path):
+        x = inputs[:,0]
+        y = inputs[:,1]
+        gp = SVGP.load(100, path )
+        gp.plot(inputs, targets, self.data_path + 'posterior.png')
+        gp.save_posterior(100, min(x), max(x), min(y), max(y), self.data_path + 'posterior.npy', verbose=False)
 
 class create_particle():
-    def __init__(self, gp_path):
-        # self.gp = gp.SVGP(1000) # Does not work
-        self.gp = gp.SVGP.load(1000, gp_path)
+    def __init__(self):
+        self.gp = SVGP(100) 
+        # self.gp = SVGP.load(1000, gp_path)
+
+
+
+
+
 
 if __name__ == '__main__':
     Train_gps()

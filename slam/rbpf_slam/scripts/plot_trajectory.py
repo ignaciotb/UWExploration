b05f55943eaa94530b6ce1d57127dd63a9350e49
stineolsson <stine@kth.se>
2021-05-01 14:38:04 +0200

    playing with colors to plot trajectory

--- slam/rbpf_slam/scripts/plot_trajectory.py
+++ slam/rbpf_slam/scripts/plot_trajectory.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from matplotlib.cm import get_cmap
 import csv
 import time
 import numpy as np
@@ -19,10 +20,18 @@ n = len(name_obs)
 # p_tr = [None]*n
 # p_obs = [None]*n
 first_obs = True # to get legend
+# col = iter(cm.rainbow(np.linspace(0,1,n)))
+name = 'Pastel1'  # 'Set3' #'tab20c' # 'Pastel1' #"Accent" 'tab20'
+cmap = get_cmap(name)  # type: matplotlib.colors.ListedColormap
+col = cmap.colors  # type: list
+k = 0
 
 for i in range(n): 
     p_obs = np.load(obs_path + name_obs[i])
     p_tr = np.load(tr_path + name_tr[i])
+    if k >= len(col):
+        k=0
+    # c = next(col)
 
     x_obs = p_obs[:,0]
     y_obs = p_obs[:,1]
@@ -33,7 +42,9 @@ for i in range(n):
         first_obs = False
     else:
         plt.plot(x_obs, y_obs , color = 'black')
-    plt.plot(x_tr, y_tr ) #, label = 'trajectory' )
+    plt.plot(x_tr, y_tr, color = col[k] ) #, label = 'trajectory' )
+    k += 1
+    
 
 plt.legend()
 plt.xlabel('x axis (m)')

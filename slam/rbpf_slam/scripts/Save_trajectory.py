9b596dd8e845d2c570839a494c7152482c5d366a
stineolsson <stine@kth.se>
2021-05-18 11:19:17 +0200

    save trajectory with keyboard

---
+++ slam/rbpf_slam/scripts/Save_trajectory.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Bool
+
+class keyboard_trajectory():
+    def __init__(self): 
+        # Control when to save trajectory
+        pub_ = rospy.Publisher('/keyboard_trajectory', Bool, queue_size=1)
+        print('(Press q to quit)')
+        msg = Bool()
+        msg.data = True
+        
+        while True:
+            v = input('Press any key to save down trajectory ...')
+            if v == 'q':
+                break
+            pub_.publish(msg)
+            print('Trajectory will be saved.')
+            
+        # rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('Save_trajectory', disable_signals=False)
+    try:
+        keyboard_trajectory()
+    except rospy.ROSInterruptException:
+        rospy.logerr("Couldn't launch rbpf_node")
+        pass

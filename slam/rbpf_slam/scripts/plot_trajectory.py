135f2b59110da0048e5dc66efde6dd3e3a1bb4e5
stineolsson <stine@kth.se>
2021-05-25 11:09:05 +0200

    resampling and saving trajectory works

--- slam/rbpf_slam/scripts/plot_trajectory.py
+++ slam/rbpf_slam/scripts/plot_trajectory.py
@@ -9,11 +9,12 @@ import time
 import numpy as np
 import os
 
-root = '/home/stine/catkin_ws/src/UWExploration/slam/rbpf_slam/data/results/'
+root = '/home/stine/catkin_ws/src/UWExploration/slam/rbpf_slam/data/results/trajectory6/'
+_, dir_name, _ = next(os.walk(root)) 
+# dir_name.remove('gp_plot')
 
 def p_path():
-    _, dir_name, _ = next(os.walk(root)) 
-    dir_name.remove('gp_plot')
+    
     k = 0
     first_obs = True # to get legend
     for p in dir_name:
@@ -56,46 +57,53 @@ def p_path():
     plt.title(name)
     plt.show()
 
-def mapping(plot_type):
-    map_path = root + 'mapping/'
-    _, _, name_est = next(os.walk(map_path + 'est_map/'))
-    _, _, name_real = next(os.walk(map_path + 'real_map/'))
-
-    n = len(name_est)
+def mapping(plot_type, Xd):
     fig = plt.figure()
-    # ax1 = fig.axes(projection='3d')
-    ax1 = fig.add_subplot(1, 2, 1)#, projection='3d')
-    ax2 = fig.add_subplot(1, 2, 2)#, projection='3d')
-    for i in range(n): 
-        pm = np.load(map_path + 'est_map/' + name_est[i])
-        X = pm[:,0]
-        Y = pm[:,1]
-        Z = pm[:,2]
-        rm = np.load(map_path + 'real_map/' + name_real[i])
-        x = rm[:,0]
-        y = rm[:,1]
-        z = rm[:,2]
+    if Xd == '3d':
+        # ax1 = fig.axes(projection='3d')
+        ax1 = fig.add_subplot(1, 2, 1, projection='3d')
+        ax2 = fig.add_subplot(1, 2, 2, projection='3d')
+    else:
+        ax1 = fig.add_subplot(1, 2, 1)#, projection='3d')
+        ax2 = fig.add_subplot(1, 2, 2)#, projection='3d')
 
-        # debug
-        # print('exp ', Y)
-        # print('real ', y)
+    for p in dir_name:
+        map_path = root + p  + '/mapping/'
+        _, _, name_est = next(os.walk(map_path + 'est_map/'))
+        _, _, name_real = next(os.walk(map_path + 'real_map/'))
 
-        if plot_type == 'scatter':
-            ax1.scatter(X, Y, c=Z, cmap='viridis', linewidth=0.5)
-            ax2.scatter(x, y, c=z, cmap='viridis', linewidth=0.5)
-        elif plot_type == 'surf':
-            ax1.plot_trisurf(X, Y, Z, cmap='viridis', edgecolor='none')
-            ax2.plot_trisurf(x, y, z, cmap='viridis', edgecolor='none')
-        elif plot_type == '2dZ':
-            ax1.plot_surface(X, Y, Z, rstride=1, cstride=1,
-                            cmap='viridis', edgecolor='none')
-            ax2.plot_surface(x, y, z, rstride=1, cstride=1,
-                            cmap='viridis', edgecolor='none')
-        # plt.show()
+        n = len(name_est)
+        
+        for i in range(n): 
+            pm = np.load(map_path + 'est_map/' + name_est[i])
+            X = pm[:,0]
+            Y = pm[:,1]
+            Z = pm[:,2]
+            rm = np.load(map_path + 'real_map/' + name_real[i])
+            x = rm[:,0]
+            y = rm[:,1]
+            z = rm[:,2]
 
-    # xx, yy = np.meshgrid(x_tr, y_tr, sparse=True)
-    # z = np.sin(xx**2 + yy**2) / (xx**2 + yy**2)
-    # h = plt.contourf(x_tr,y_tr,z)
+            # debug
+            # print('exp ', Y)
+            # print('real ', y)
+
+            if plot_type == 'scatter':
+                ax1.scatter(X, Y, c=Z, cmap='viridis', linewidth=0.5)
+                ax2.scatter(x, y, c=z, cmap='viridis', linewidth=0.5)
+            elif plot_type == 'surf': # only with 3d
+                ax1.plot_trisurf(X, Y, Z, cmap='viridis', edgecolor='none')
+                ax2.plot_trisurf(x, y, z, cmap='viridis', edgecolor='none')
+            elif plot_type == '2dZ': # z must be 2-dimensional
+                ax1.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                                cmap='viridis', edgecolor='none')
+                ax2.plot_surface(x, y, z, rstride=1, cstride=1,
+                                cmap='viridis', edgecolor='none')
+            # plt.show()
+
+        # xx, yy = np.meshgrid(x_tr, y_tr, sparse=True)
+        # z = np.sin(xx**2 + yy**2) / (xx**2 + yy**2)
+        # h = plt.contourf(x_tr,y_tr,z)
     ax1.set_title('RBPF map')
     ax2.set_title('Real map')
     ax1.set_xlabel('x axis (m)')
@@ -147,4 +155,4 @@ def mapping(plot_type):
 
 if __name__ == '__main__':
     p_path()
-    # mapping('scatter')
+    mapping('scatter', '2d')

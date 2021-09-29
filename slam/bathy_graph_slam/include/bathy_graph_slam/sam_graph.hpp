311dab49873e768f2bef754adb2977cb6e97f7c3
ignaciotb <ignaciotorroba@hotmail.com>
2021-09-29 14:29:05 +0200

    Removed unnecessary deps from the SAM graph class

--- slam/bathy_graph_slam/include/bathy_graph_slam/sam_graph.hpp
+++ slam/bathy_graph_slam/include/bathy_graph_slam/sam_graph.hpp
@@ -3,49 +3,56 @@
 
 #include <gtsam/geometry/Pose2.h>
 #include <gtsam/geometry/Point2.h>
-
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/geometry/Point3.h>
 #include <gtsam/inference/Symbol.h>
-
 #include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/slam/StereoFactor.h>
 #include <gtsam/sam/BearingRangeFactor.h>
-
+#include <gtsam/geometry/BearingRange.h>
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
 #include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
 #include <gtsam/nonlinear/Marginals.h>
 #include <gtsam/nonlinear/Values.h>
-
-#include <gtsam/slam/dataset.h>
-#include <gtsam/nonlinear/Values.h>
-
 #include <gtsam/nonlinear/NonlinearISAM.h>
 #include <gtsam/nonlinear/ISAM2.h>
 
-#include <ros/ros.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 
 using namespace gtsam;
 
-class samGraph{
-    public:
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloudT;
+typedef pcl::PointXYZ PointT;
 
+class samGraph
+{
+    typedef BearingRangeFactor<Pose3, Point3> BearingRangeFactor3D;
+
+public:
     samGraph();
 
     ~samGraph();
 
-    void addPrior();
+    void addPrior(Pose3& initPose);
 
-    void addOdomFactor(Pose2 odom_step, size_t step);
+    void addOdomFactor(Pose3 factor_pose, Pose3 odom_step, size_t step);
 
-    // void initializeISAM();
+    void addLandmarksFactor(PointCloudT&, size_t step, 
+                            std::vector<int>& lm_idx, Pose3 submap_pose);
+
+    void updateISAM2();
 
     // Create a factor graph
-    NonlinearFactorGraph::shared_ptr newFactors_;
+    NonlinearFactorGraph::shared_ptr graph_;
     Values::shared_ptr initValues_;
     boost::shared_ptr<ISAM2> isam_;
-    Pose2 lastPose_;
+    Pose3 lastPose_;
+    int num_landmarks_;
 
     // TODO: this has to be an input parameter
-    SharedDiagonal odoNoise; 
+    SharedDiagonal odoNoise_;
+    SharedDiagonal brNoise_;
 };
-
 
 #endif

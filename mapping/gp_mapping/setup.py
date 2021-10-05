5ca9b72527655f98610d67c091ba555ea1c16be7
ignaciotb <ignaciotorroba@hotmail.com>
2021-10-05 18:40:35 +0200

    Added gpytorch mapping lib as ros pkg

---
+++ mapping/gp_mapping/setup.py
@@ -0,0 +1,21 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['gp_mapping'],
+    package_dir={'': 'src'},
+)
+
+setup(**setup_args)
+
+#  from setuptools import setup
+  #
+#  setup(name="bathymetric_svgp",
+      #  version="0.0.0",
+      #  packages=["gp_mapping"],
+      #  package_dir={'': 'src'},
+      #  )
+

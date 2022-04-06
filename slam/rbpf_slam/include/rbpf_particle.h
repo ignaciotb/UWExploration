e4f066155665427900b39b427742b0abce9fc76a
marcoCella <mcella@live.it>
2022-04-06 17:21:48 +0200

    [RBPFCPP] Better structure and some callback functions added

---
+++ slam/rbpf_slam/include/rbpf_particle.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <math.h>
+#include <ros/ros.h>
+
+#include <Eigen/Dense>
+#include <Eigen/Core>
+#include <eigen_conversions/eigen_msg.h>
+#include <tf_conversions/tf_eigen.h>
+
+#include <std_msgs/Header.h>
+
+#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+#include <sensor_msgs/PointField.h>
+
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.h>
+
+#include <geometry_msgs/Point32.h>
+
+using namespace std;
+
+sensor_msgs::PointCloud2 pack_cloud(string frame, std::vector<Eigen::RowVector3f> mbes);
+
+std::vector<geometry_msgs::Point32> pcloud2ranges_full(sensor_msgs::PointCloud2 point_cloud);
+
+// std::vector linspace(float start, float end, float num);

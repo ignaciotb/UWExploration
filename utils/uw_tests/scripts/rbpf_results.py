c0900538948aaa283eb42bb7f9c42309384f767c
marcoCella <mcella@live.it>
2022-07-14 17:09:53 +0200

    Script for avg trajectory and map running

--- utils/uw_tests/scripts/rbpf_results.py
+++ utils/uw_tests/scripts/rbpf_results.py
@@ -10,47 +10,50 @@ import matplotlib.pyplot as plt
 from scipy.spatial.transform import Rotation as rot
 
 
-def plot_rbpf_solution(avg_pos_0, avg_or_0, avg_pos_29, avg_or_29, data_0, data_29):
+# plot_rbpf_solution(avg_pos_out, avg_ors_out, data_out, avg_pos_in, avg_ors_in, data_in)
 
-    # print(avg_pos_0.shape)
-    # print(beams_0.shape)
+
+def plot_rbpf_solution(avg_pos_out, avg_ors_out, pos_in, ors_in, data_in):
 
     i = 0
     j = 0
-    rot_mat_0 = avg_or_0[i]
-    rot_mat_29 = avg_or_29[i]
+    rot_mat_avg = avg_ors_out[i]
+    rot_mat_in = ors_in[i]
 
-    beams_29 = data_29["beams"]
-    beams_0 = data_0["beams"]
+    beams_in = data_in["beams"]
     beams_tf = []
-    for beam in beams_29:
-        if j == 100:
+    for beam in beams_in:
+        if j == 100:    # Number of beams per ping used in survey
             j = 0
             i += 1
-            rot_mat_0 = avg_or_0[i]
-            rot_mat_29 = avg_or_29[i]
+            rot_mat_avg = avg_ors_out[i]
+            rot_mat_in = ors_in[i]
 
         # Back to map origin
-        beam = np.add(beam.T, -avg_pos_29[i])
-        beam = np.matmul(rot_mat_29.T, beam)
+        beam = np.add(beam.T, -pos_in[i])
+        beam = np.matmul(rot_mat_in.T, beam)
 
         # Transform to avg pose
-        beam = np.matmul(rot_mat_0, beam)
-        beam = np.add(beam.T, avg_pos_0[i])
+        beam = np.matmul(rot_mat_avg, beam.T)
+        beam = np.add(beam.T, avg_pos_out[i])
         beams_tf.append(beam)
         j += 1
 
     beams_tf = np.asarray(beams_tf).reshape((-1, 3))
+    
     fig, ax = plt.subplots(1, sharex=True, sharey=True)
-    # cr = ax.scatter(beams_tf[:, 0], beams_tf[:, 1], c=beams_tf[:, 2],
+
+    # # For testing
+    # beams_in = data_in["beams"]
+    # cr = ax.scatter(beams_in[:, 0], beams_in[:, 1], c=beams_in[:, 2],
     #                     cmap='jet', s=0.4, edgecolors='none')
 
-    cr = ax.scatter(beams_0[:, 0], beams_0[:, 1], c=beams_0[:, 2],
+    cr = ax.scatter(beams_tf[:, 0], beams_tf[:, 1], c=beams_tf[:, 2],
                         cmap='jet', s=0.4, edgecolors='none')
+    
     fig.colorbar(cr, ax=ax)
-
-    ax.plot(avg_pos_0[:,0], avg_pos_0[:,1], "-r", linewidth=0.2)
-    ax.plot(avg_pos_29[:,0], avg_pos_29[:,1], "-b", linewidth=0.2)
+    ax.plot(avg_pos_out[:,0], avg_pos_out[:,1], "-r", linewidth=0.2)
+    ax.plot(pos_in[:,0], pos_in[:,1], "-b", linewidth=0.2)
 
     fig.savefig("./rbpf_map.png", bbox_inches='tight', dpi=1000)
 
@@ -59,86 +62,74 @@ if __name__ == '__main__':
 
     i = sys.argv[1]    
     path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'rbpf/lolo_0/' + str(i)))
-    particle_count = 1
+    particle_count = 30
 
-    # Path(results_dir).mkdir(parents=True, exist_ok=False)
     positions_all = []
     orientations_all = []
-    # for file in os.listdir(path):
-    #     if file.endswith(".npz") and not file.startswith("gt"):
-    #         # print(os.path.join(path, file))
-    #         data = np.load(os.path.join(path, file))
-    #         positions_all.append(data["track_position"])
-    #         print(data["track_position"].shape)
-    #         orientations_all.append(data["track_orientation"])
-
-    data_0 = np.load(os.path.join(path, "data_particle_0.npz"))
-    positions_all.append(data_0["track_position"])
-    orientations_all.append(data_0["track_orientation"])
-    
+    i = 0
+    for file in os.listdir(path):
+        if file.endswith(".npz") and not file.startswith("gt") and i < particle_count:
+            print(os.path.join(path, file))
+            data = np.load(os.path.join(path, file))
+            positions_all.append(data["track_position"])
+            orientations_all.append(data["track_orientation"])
+            i += 1
+   
     positions = np.asarray(positions_all)
-    positions = np.reshape(positions, (-1,particle_count*3)) 
-    
     orientations = np.asarray(orientations_all)
-    orientations = np.reshape(orientations, (orientations.shape[1], 3)) 
-    orientations = np.reshape(orientations, (-1, 9)) 
-    print(orientations)
+    positions_arranged = positions[0]
+    orientations_arranged = orientations[0]
+
+    for i in range(1, positions.shape[0]):
+        positions_arranged = np.hstack((positions_arranged, positions[i]))
+        orientations_arranged = np.hstack((orientations_arranged, orientations[i]))
+    
+    # Eigen decomposition of Q*Qt for quaternions
+    avg_positions = []
+    avg_or = []
+    for row in range(0, positions_arranged.shape[0]):
+    # for row in range(0, 1):
+        # Positions
+        step_pos = positions_arranged[row, :].reshape(-1, 3).sum(0)/particle_count
+        avg_positions.append(step_pos)
+
+        # Orientations: need to be converted to quaternions for averaging
+        ors_col = orientations_arranged[row, :].reshape(-1, 3)
+        ors_col_quats = np.zeros((1,4))
+        for row_i in ors_col:
+            ors_col_quats = np.vstack((ors_col_quats, rot.from_euler("xyz", row_i, degrees=False).as_quat()))
+
+        if True:
+            ors_col_quats_new = ors_col_quats[1:,:]
+            # Eigen decomposition of matrix of quats
+            w, v = np.linalg.eigh(np.matmul(ors_col_quats_new.T, ors_col_quats_new))
+            idx_max = np.where(w == w.max())
+            avg_or.append(rot.from_quat(v.T[3]).as_matrix())
+        else:
+            # Left here for the memory of it
+            ors_col_quats = ors_col_quats.sum(0)/particle_count
+            avg_or.append(rot.from_quat(ors_col_quats).as_matrix())
+
+    avg_pos = np.asarray(avg_positions)
+    avg_ors = np.asarray(avg_or)
+
+    #### Use one single particle's data to be transformed to computed average trajectory
+    positions_in = []
+    orientations_in = []
+    positions_in.append(data["track_position"])
+    orientations_in.append(data["track_orientation"])
+    
+    positions = np.asarray(positions_in)
+    positions = np.reshape(positions, (-1, 3)) 
+    orientations = np.asarray(orientations_in)
+    orientations = np.reshape(orientations, (-1, 3)) 
 
     avg_or = []
     for ori in orientations:
-        rot_mat_0 = ori.reshape(3,3).T
-        avg_or.append(rot_mat_0)
-        # print(rot_mat_0)
+        rot_avg = rot.from_euler("xyz", ori, degrees=False).as_matrix()
+        avg_or.append(rot_avg)
 
+    pos_in = positions
+    ors_in = np.asarray(avg_or)
 
-
-    # # Average poses of particles
-    avg_positions = []
-    # avg_or = []
-    for row in range(0, positions.shape[0]):
-        step_pos = positions[row, :].reshape(-1, 3).sum(0)/particle_count
-        avg_positions.append(step_pos)
-
-    #     step_or = orientations[row, :].reshape(-1, 3).sum(0)/particle_count
-    #     avg_or.append(step_or)
-
-    avg_pos_0 = np.asarray(avg_positions)
-    avg_ors_0 = np.asarray(avg_or)
-    # print(avg_pos)
-    # print(avg_ors)
-
-    # # Load particle 0 data
-    positions_all = []
-    orientations_all = []
-    data_29 = np.load(os.path.join(path, "data_particle_29.npz"))
-    positions_all.append(data_29["track_position"])
-    orientations_all.append(data_29["track_orientation"])
-    
-    positions = np.asarray(positions_all)
-    positions = np.reshape(positions, (-1,particle_count*3)) 
-    
-    orientations = np.asarray(orientations_all)
-    orientations = np.reshape(orientations, (orientations.shape[1], 3)) 
-    orientations = np.reshape(orientations, (-1, 9)) 
-    print(orientations)
-
-    avg_or = []
-    for ori in orientations:
-        rot_mat_0 = ori.reshape(3,3).T
-        avg_or.append(rot_mat_0)
-        # print(rot_mat_0)
-
-    # # Average poses of particles
-    avg_positions = []
-    # avg_or = []
-    for row in range(0, positions.shape[0]):
-        step_pos = positions[row, :].reshape(-1, 3).sum(0)/particle_count
-        avg_positions.append(step_pos)
-
-    #     step_or = orientations[row, :].reshape(-1, 3).sum(0)/particle_count
-    #     avg_or.append(step_or)
-
-    avg_pos_29 = np.asarray(avg_positions)
-    avg_ors_29 = np.asarray(avg_or)
-
-    plot_rbpf_solution(avg_pos_0, avg_ors_0, avg_pos_29, avg_ors_29, data_0, data_29)
+    plot_rbpf_solution(avg_pos, avg_ors, pos_in, ors_in, data)

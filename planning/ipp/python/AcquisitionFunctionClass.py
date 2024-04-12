f36a808671b82ff3216dbd29d90958904e0fd39a
alwaki <xalwa999@gmail.com>
2024-04-12 16:45:57 +0200

    working on adding dubins

--- planning/ipp/python/AcquisitionFunctionClass.py
+++ planning/ipp/python/AcquisitionFunctionClass.py
@@ -22,37 +22,75 @@ class UCB_custom(UpperConfidenceBound):
             A `(b1 x ... bk)`-dim tensor of Upper Confidence Bound values at the
             given design points `X`.
         """
-        mean, sigma = self._mean_and_sigma(X)
+        # Split suggested planar coordinates and headings
+        xy = X[:,:,:2]
+        theta = X[:,:,2]
+
+        # Get the reward along path associated with travelling to candidates
+        rewards = self._dubins_swath(xy, theta)
+        print(X)
+        print(rewards)
+        mean, sigma = self._mean_and_sigma(xy)
         ucb = (mean if self.maximize else -mean) + self.beta.sqrt() * sigma
-        cost = self._travel_cost(X, self.current_state)
-        reward = torch.div(ucb, cost)
-        return ucb
+        print(ucb)
+        return rewards
     
-    def _travel_cost(self, X: Tensor, current_pose) -> Tuple[Tensor, Optional[Tensor]]:
-        """Computes the first and second moments of the model posterior.
+    def _dubins_swath(self, xy, theta) -> Tuple[Tensor, Optional[Tensor]]:
+        """ Computes the dubins path to the candidates. Generates points
+            along the line to be used for posterior sampling, and calculates
+            the cost of the path as the length.
 
         Args:
-            X: `batch_shape x q x d`-dim Tensor of model inputs.
-            current_pose (_type_): _description_
+            xy: `batch_shape x q x (d-1)`-dim Tensor of model inputs.
+            theta: `batch_shape x q x 1`-dim Tensor of model inputs.
 
         Returns:
             A tuple of tensors containing the cost of travelling. 
             Removes the last two dimensions if they have size one.
         """
-        destinations = (X.squeeze(-2).squeeze(-1)).detach().numpy()
+        destinations = (xy.squeeze(-2).squeeze(-1))
+        angles = (theta.squeeze(-2).squeeze(-1))
         #print(destinations)
-        wp_resolution = 0.5
+        wp_resolution = 2
         turning_radius = 5
-        costs = Tensor()
-        for place in destinations:
-            possible_paths = []
-            for angle in np.linspace(0, 6.28, 4):
-                path = dubins.shortest_path(current_pose, [place[0], place[1], angle], turning_radius)
-                _, length_arr = path.sample_many(wp_resolution) # _ = waypoints, if needed for future reference
-                cost = length_arr[-1] + wp_resolution
-                possible_paths.append(cost)
-            lowest_cost = min(possible_paths)
-            costs = cat((costs,Tensor([lowest_cost])),0)
-        return costs
+        rewards = Tensor()
+        for idx, place in enumerate(destinations):
+            
+            # Calculate dubins path to candidate, and travel cost
+            path = dubins.shortest_path(self.current_state, [place[0], place[1], angles[idx]], turning_radius)
+            wp_poses, length_arr = path.sample_many(wp_resolution)
+            cost = length_arr[-1] + wp_resolution
+            
+            # Get sample swath points orthogonally to path at regular intervals
+            points = self._get_orthogonal_samples(wp_poses)
+            
+            # Calculate UCB/cost reward of travelling to candidate
+            mean, sigma = self._mean_and_sigma(points)
+            mean = mean.mean()
+            sigma = sigma.mean()
+            ucb = (mean if self.maximize else -mean) + self.beta.sqrt() * sigma
+            reward = torch.div(ucb, cost)
+            rewards = cat((rewards,reward.reshape(1)),0)
+        return rewards
+    
+    def _get_orthogonal_samples(self, poses):
+        samples = Tensor()
+        for pose in poses:
+            x = pose[0]
+            y = pose[1]
+            yaw = pose[2]
+
+            dx = np.sin(yaw) # shifted by 90 degree for orthogonality
+            dy = np.cos(yaw) 
+
+            for i in np.linspace(0.2, 1, 3):
+                dx_s = dx * i
+                dy_s = dy * i
+                n1 = [x + dx_s, y - dy_s]
+                n2 = [x - dx_s, y + dy_s]
+                #all_samples.append(np.array([n1, n2]))
+                samples = cat((samples,Tensor([n1, n2])),0)
+
+        return samples
     
     

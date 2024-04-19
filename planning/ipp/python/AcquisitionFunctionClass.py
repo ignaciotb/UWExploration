972612d91403c399a1d9fa3d1f126d1b5fb5d6c3
alwaki <xalwa999@gmail.com>
2024-04-19 16:55:23 +0200

    cleaned up, fixed orthogonal sampling, docs

--- planning/ipp/python/AcquisitionFunctionClass.py
+++ planning/ipp/python/AcquisitionFunctionClass.py
@@ -7,13 +7,15 @@ from typing import Dict, Optional, Tuple, Union
 import numpy as np
 
 class UCB_custom(UpperConfidenceBound):
-    def __init__(self, model, beta, current_pose, posterior_transform = None, maximize = True, **kwargs):
+    def __init__(self, model, beta, current_pose, waypoint_sample_interval = 2, posterior_transform = None, maximize = True, **kwargs):
         super(UCB_custom, self).__init__(model, beta, posterior_transform = None, maximize = False, **kwargs)
         self.current_state = current_pose
+        self.waypoint_sample_interval = int(waypoint_sample_interval)
             
     @t_batch_mode_transform(expected_q=1)
     def forward(self, X: Tensor) -> Tensor:
-        r"""Evaluate the Upper Confidence Bound on the candidate set X.
+        r"""Evaluate the Upper Confidence Bound on the candidate set X. (note, limited to
+        q = 1, single candidate output)
 
         Args:
             X: A `(b1 x ... bk) x 1 x d`-dim batched tensor of `d`-dim design points.
@@ -28,11 +30,7 @@ class UCB_custom(UpperConfidenceBound):
 
         # Get the reward along path associated with travelling to candidates
         rewards = self._dubins_swath(xy, theta)
-        #print(X)
-        #print(rewards)
-        #mean, sigma = self._mean_and_sigma(xy)
-        #ucb = (mean if self.maximize else -mean) + self.beta.sqrt() * sigma
-        #print(ucb)
+
         return rewards
     
     def _dubins_swath(self, xy, theta) -> Tuple[Tensor, Optional[Tensor]]:
@@ -61,28 +59,41 @@ class UCB_custom(UpperConfidenceBound):
             cost = length_arr[-1] + wp_resolution
             
             # Get sample swath points orthogonally to path at regular intervals
-            points = self._get_orthogonal_samples(wp_poses)
+            points = self._get_orthogonal_samples(wp_poses[::self.waypoint_sample_interval])
             
             # Calculate UCB/cost reward of travelling to candidate
             mean, sigma = self._mean_and_sigma(points)
             mean = mean.sum()
             sigma = sigma.sum()
-            ucb = abs(mean - self.model.model.mean_module.constant) + self.beta.sqrt() * sigma
+            ucb = abs(mean - self.model.model.mean_module.constant) + self.beta.sqrt() * sigma #relative to gp mean
             reward = torch.div(ucb, cost)
             rewards = cat((rewards,reward.reshape(1)),0)
         return rewards
     
-    def _get_orthogonal_samples(self, poses):
+    def _get_orthogonal_samples(self, poses, nbr_samples=6, swath_width=5):
+        """ Generates points on lines orthogonal to a vector. Will generate
+            `nbr_samples` for each given vector, along a line of given swath width.
+        
+
+        Args:
+            poses (list): [x y theta]
+            nbr_samples (int, optional): number of samples generated for each vector. Defaults to 6.
+            swath_width (int, optional): width of line sampled from, for each vector. Defaults to 5.
+
+        Returns:
+            torch.Tensor: concatenated xy points of samples
+        """
         samples = Tensor()
+        radius = 0.5*swath_width
         for pose in poses:
             x = pose[0]
             y = pose[1]
             yaw = pose[2]
 
-            dx = 4*np.sin(yaw) # shifted by 90 degree for orthogonality
-            dy = 4*np.cos(yaw) 
+            dx = radius*np.sin(yaw) # shifted by 90 degree for orthogonality
+            dy = radius*np.cos(yaw) 
 
-            for i in np.linspace(0.2, 1, 3):
+            for i in np.linspace(-1, 1, nbr_samples):
                 dx_s = dx * i
                 dy_s = dy * i
                 n1 = [x + dx_s, y - dy_s]

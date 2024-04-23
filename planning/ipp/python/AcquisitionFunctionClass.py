3e4bd4acee7c93e21e01eea3fa6183aefbf02439
alwaki <xalwa999@gmail.com>
2024-04-23 19:16:05 +0200

    added qUCB

--- planning/ipp/python/AcquisitionFunctionClass.py
+++ planning/ipp/python/AcquisitionFunctionClass.py
@@ -1,14 +1,103 @@
-from botorch.acquisition import UpperConfidenceBound
+from botorch.acquisition import UpperConfidenceBound, MCAcquisitionFunction
 from botorch.utils.transforms import t_batch_mode_transform
 from torch import Tensor, cat
 import torch
 import dubins
 from typing import Dict, Optional, Tuple, Union
 import numpy as np
+import math
+from typing import Any, Optional, Union
+from botorch.acquisition.objective import (
+    MCAcquisitionObjective,
+    PosteriorTransform,
+)
+from botorch.models.model import Model
+from botorch.sampling.base import MCSampler
+from botorch.utils.transforms import (
+    concatenate_pending_points,
+    t_batch_mode_transform,
+)
+from torch import Tensor
 
-class UCB_custom(UpperConfidenceBound):
+class qUCB_xy(MCAcquisitionFunction):
+    r"""MC-based batch Upper Confidence Bound.
+
+    Uses a reparameterization to extend UCB to qUCB for q > 1 (See Appendix A
+    of [Wilson2017reparam].)
+
+    `qUCB = E(max(mu + |Y_tilde - mu|))`, where `Y_tilde ~ N(mu, beta pi/2 Sigma)`
+    and `f(X)` has distribution `N(mu, Sigma)`.
+
+    Example:
+        >>> model = SingleTaskGP(train_X, train_Y)
+        >>> sampler = SobolQMCNormalSampler(1024)
+        >>> qUCB = qUpperConfidenceBound(model, 0.1, sampler)
+        >>> qucb = qUCB(test_X)
+    """
+
+    def __init__(
+        self,
+        model: Model,
+        beta: float,
+        sampler: Optional[MCSampler] = None,
+        objective: Optional[MCAcquisitionObjective] = None,
+        posterior_transform: Optional[PosteriorTransform] = None,
+        X_pending: Optional[Tensor] = None,
+    ) -> None:
+        r"""q-Upper Confidence Bound.
+
+        Args:
+            model: A fitted model.
+            beta: Controls tradeoff between mean and standard deviation in UCB.
+            sampler: The sampler used to draw base samples. See `MCAcquisitionFunction`
+                more details.
+            objective: The MCAcquisitionObjective under which the samples are
+                evaluated. Defaults to `IdentityMCObjective()`.
+            posterior_transform: A PosteriorTransform (optional).
+            X_pending: A `batch_shape x m x d`-dim Tensor of `m` design points that have
+                points that have been submitted for function evaluation but have not yet
+                been evaluated. Concatenated into X upon forward call. Copied and set to
+                have no gradient.
+        """
+        super().__init__(
+            model=model,
+            sampler=sampler,
+            objective=objective,
+            posterior_transform=posterior_transform,
+            X_pending=X_pending,
+        )
+        self.beta_prime = math.sqrt(beta * math.pi / 2)
+
+    @concatenate_pending_points
+    @t_batch_mode_transform()
+    def forward(self, X: Tensor) -> Tensor:
+        r"""Evaluate qUpperConfidenceBound on the candidate set `X`.
+
+        Args:
+            X: A `batch_sahpe x q x d`-dim Tensor of t-batches with `q` `d`-dim design
+                points each.
+
+        Returns:
+            A `batch_shape'`-dim Tensor of Upper Confidence Bound values at the given
+            design points `X`, where `batch_shape'` is the broadcasted batch shape of
+            model and input `X`.
+        """
+        posterior = self.model.posterior(
+            X=X, posterior_transform=self.posterior_transform
+        )
+        samples = self.get_posterior_samples(posterior)
+        obj = self.objective(samples, X=X)
+        mean = obj.mean(dim=0)
+        ucb_samples = mean + self.beta_prime * (obj - mean).abs()
+        return ucb_samples.max(dim=-1)[0].mean(dim=0)
+    
+    
+
+
+
+class UCB_path(UpperConfidenceBound):
     def __init__(self, model, beta, current_pose, waypoint_sample_interval = 2, posterior_transform = None, maximize = True, **kwargs):
-        super(UCB_custom, self).__init__(model, beta, posterior_transform = None, maximize = False, **kwargs)
+        super(UCB_path, self).__init__(model, beta, posterior_transform = None, maximize = False, **kwargs)
         self.current_state = current_pose
         self.waypoint_sample_interval = int(waypoint_sample_interval)
             

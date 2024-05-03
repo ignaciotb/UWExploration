bab323f1cec9b9a2e7075e03e64e1bd59fa5a952
alwaki <xalwa999@gmail.com>
2024-05-03 16:38:27 +0200

    added upstreaming of parameters to launch

--- planning/ipp/python/planner_node.py
+++ planning/ipp/python/planner_node.py
@@ -14,16 +14,25 @@ if __name__ == "__main__":
     
     rospy.init_node("AUV_path_planner_node")
     
-    choice          = rospy.get_param("~planner_type")
-    tr              = rospy.get_param("~turning_radius")
-    corner_topic    = rospy.get_param("~corner_topic")
-    path_topic      = rospy.get_param("~path_topic")
-    swath_width     = rospy.get_param("~swath_width")
-    swath_overlap   = rospy.get_param("~swath_overlap")
-    bound_left      = rospy.get_param("~bound_left")
-    bound_right     = rospy.get_param("~bound_right")
-    bound_up        = rospy.get_param("~bound_up")
-    bound_down      = rospy.get_param("~bound_down")
+    # Get parameters from ROS
+    choice              = rospy.get_param("~planner_type")
+    turn_radius         = rospy.get_param("~turning_radius")
+    corner_topic        = rospy.get_param("~corner_topic")
+    path_topic          = rospy.get_param("~path_topic")
+    swath_width         = rospy.get_param("~swath_width")
+    swath_overlap       = rospy.get_param("~swath_overlap")
+    bound_left          = rospy.get_param("~bound_left")
+    bound_right         = rospy.get_param("~bound_right")
+    bound_up            = rospy.get_param("~bound_up")
+    bound_down          = rospy.get_param("~bound_down")
+    bound_down          = rospy.get_param("~bound_down")
+    train_rate          = rospy.get_param("~bound_down")
+    wp_resolution       = rospy.get_param("~bound_down")
+    path_nbr_samples    = rospy.get_param("~bound_down")
+    voxel_size          = rospy.get_param("~bound_down")
+    wp_sample_interval  = rospy.get_param("~bound_down")
+    horizon_distance    = rospy.get_param("~horizon_distance")
+    border_margin       = rospy.get_param("~border_margin")
     
     bounds = [bound_left, bound_right, bound_up, bound_down]
     
@@ -32,13 +41,17 @@ if __name__ == "__main__":
         if choice == "lawnmower":
             rospy.loginfo("Initializing planner node! Using Lawnmower pattern.")  
             planner = SimplePlanner(corner_topic=corner_topic, path_topic=path_topic, bounds=bounds, 
-                                    turning_radius=tr, sw=swath_width, so=swath_overlap)
+                                    turning_radius=turn_radius, training_rate=train_rate, sw=swath_width, 
+                                    so=swath_overlap)
         
-        # Run bayesian planner 
+        # Run bayesian optimization based planner 
         else:
             rospy.loginfo("Initializing planner node! Using Bayesian Optimization.")  
             planner = BOPlanner(corner_topic=corner_topic, path_topic=path_topic, bounds=bounds, 
-                                turning_radius=tr)
+                                turning_radius=turn_radius, training_rate=train_rate, wp_resolution=wp_resolution,
+                                swath_width=swath_width, path_nbr_samples=path_nbr_samples, voxel_size=voxel_size,
+                                wp_sample_interval=wp_sample_interval, horizon_distance=horizon_distance,
+                                border_margin=border_margin)
             
     except rospy.ROSInterruptException:
         rospy.logerr('Could not launch AUV path planner node')

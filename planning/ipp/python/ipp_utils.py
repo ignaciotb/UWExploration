ed1d0b2c21349e2c257296fca34af391439d31b9
alwaki <xalwa999@gmail.com>
2024-05-24 13:20:16 +0200

    added pack_cloud, also simulated points now return as numpy arr

--- planning/ipp/python/ipp_utils.py
+++ planning/ipp/python/ipp_utils.py
@@ -1,5 +1,9 @@
 import numpy as np
 import torch
+from sensor_msgs.msg import PointCloud2, PointField
+from std_msgs.msg import Header
+import rospy
+from sensor_msgs import point_cloud2
 
 def generate_local_bounds(global_bounds, vehicle_position, horizon_distance, safety_margin):
     """ To plan in a local area rather than the entire survey area, this function can generate bounds
@@ -53,7 +57,7 @@ def get_orthogonal_samples(poses, nbr_samples=10, swath_width=20):
         swath_width (float, optional): width of line sampled from, for each vector. Defaults to 20.
 
     Returns:
-        torch.Tensor: concatenated xy points of samples
+        np.array: concatenated xy points of samples with dimensions [len(poses) * nbr_samples, 2]
     """
     samples = np.zeros((len(poses), nbr_samples, 2))
     radius = 0.5*swath_width
@@ -69,7 +73,8 @@ def get_orthogonal_samples(poses, nbr_samples=10, swath_width=20):
     dy_s = np.expand_dims(dy, 1) * op + np.expand_dims(y, 1)
     samples[:,:,0] = dx_s 
     samples[:,:,1] = dy_s
-    return torch.from_numpy(samples).type(torch.FloatTensor)
+    output = np.reshape(samples, (len(poses) * nbr_samples, 2))
+    return output #torch.from_numpy(output).type(torch.FloatTensor)
 
 def save_model(model, filename):
         """ Saves a torch model
@@ -90,3 +95,16 @@ def load_model(model, filename):
     cp = torch.load(filename)
     model.load_state_dict(cp['model'])
     return model
+
+def pack_cloud(frame, mbes):
+    mbes_pcloud = PointCloud2()
+    header = Header()
+    header.stamp = rospy.Time.now()
+    header.frame_id = frame
+    fields = [PointField('x', 0, PointField.FLOAT32, 1),
+              PointField('y', 4, PointField.FLOAT32, 1),
+              PointField('z', 8, PointField.FLOAT32, 1)]
+
+    mbes_pcloud = point_cloud2.create_cloud(header, fields, mbes)
+
+    return mbes_pcloud

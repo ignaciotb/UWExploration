ed1c1326c427ddcd2109bfc860a968ec052a8a1d
alwaki <xalwa999@gmail.com>
2024-05-31 18:14:39 +0200

    removed unused params

--- planning/ipp/python/planner_node.py
+++ planning/ipp/python/planner_node.py
@@ -34,12 +34,7 @@ if __name__ == "__main__":
     wp_sample_interval  = rospy.get_param("~wp_sample_interval")
     horizon_distance    = rospy.get_param("~horizon_distance")
     border_margin       = rospy.get_param("~border_margin")
-    vehicle_velocity    = rospy.get_param("~vehicle_velocity")
-    max_time            = rospy.get_param("~max_mission_time")
-    beta                = rospy.get_param("~beta")
-    MCTS_begin_time     = rospy.get_param("~MCTS_begin_time")
-    MCTS_interrupt_time = rospy.get_param("~MCTS_interrupt_time")
-    
+    beta                = rospy.get_param("~beta")    
 
     low_x = min(bound_left, bound_right)
     high_x = max(bound_left, bound_right)
@@ -63,8 +58,8 @@ if __name__ == "__main__":
             rospy.loginfo("Initializing planner node! Using Lawnmower pattern.")  
             planner = LawnmowerPlannerClass.LMPlanner(corner_topic=corner_topic, path_topic=path_topic, 
                                     planner_req_topic=planner_req_topic, odom_topic=odom_topic,
-                                    bounds=bounds, vehicle_velocity=vehicle_velocity, max_time=max_time,
-                                    turning_radius=turn_radius, training_rate=train_rate, sw=swath_width)
+                                    bounds=bounds, turning_radius=turn_radius, training_rate=train_rate, 
+                                    sw=swath_width)
         
         # Run bayesian optimization based planner 
         else:
@@ -73,9 +68,7 @@ if __name__ == "__main__":
                                 odom_topic=odom_topic,bounds=bounds, turning_radius=turn_radius, training_rate=train_rate, 
                                 wp_resolution=wp_resolution, swath_width=swath_width, path_nbr_samples=path_nbr_samples, 
                                 voxel_size=voxel_size, wp_sample_interval=wp_sample_interval, horizon_distance=horizon_distance,
-                                border_margin=border_margin, beta=beta, 
-                                vehicle_velocity=vehicle_velocity, max_time=max_time, MCTS_begin_time=MCTS_begin_time,
-                                MCTS_interrupt_time=MCTS_interrupt_time)
+                                border_margin=border_margin, beta=beta)
             
     except rospy.ROSInterruptException:
         rospy.logerr('Could not launch AUV path planner node')
